0fbe9393f579c45df202d7067a960da1be5cff1d
TheGoldenPatrik1 <55322484+TheGoldenPatrik1@users.noreply.github.com>
2023-05-13 12:52:44 -0500

    Fix mistakes

--- src/test.py
+++ src/test.py
@@ -11,12 +11,12 @@ with open('books.txt') as f:
       if arg1 >= len(book_list):
         print(f"ERROR: {arg1} is greater than length of book list ({len(book_list)})")
       else:
-        book = book_list[arg1]
+        book = books[book_list[arg1]]
         print(f"getting data for {book['title']}")
     else:
       arg1 = arg1.lower()
       if arg1 in book_list:
-        book = book_list[arg1]
+        book = books[arg1]
         print(f"getting data for {book['title']}")
       else:
         print(f"ERROR: {arg1} is not in book list")
