e928468fb8aa28dfaa5d0bd987c0741939654115
TheGoldenPatrik1 <55322484+TheGoldenPatrik1@users.noreply.github.com>
2023-05-13 13:47:52 -0500

    Test file editor

--- src/test.py
+++ src/test.py
@@ -1,22 +1,26 @@
 import sys
 import json
 
-with open('books.txt') as f:
-  books = json.load(f)
-  book_list = list(books.keys())
-  if len(sys.argv) > 1:
-    arg1 = sys.argv[1]
-    if arg1.isdigit():
-      arg1 = int(arg1)
-      if arg1 >= len(book_list):
-        print(f"ERROR: {arg1} is greater than length of book list ({len(book_list)})")
-      else:
-        book = books[book_list[arg1]]
-        print(f"getting data for {book['title']}")
-    else:
-      arg1 = arg1.lower()
-      if arg1 in book_list:
-        book = books[arg1]
-        print(f"getting data for {book['title']}")
-      else:
-        print(f"ERROR: {arg1} is not in book list")
+def add_book():
+  short_name = input("Enter the short name: ")
+  print(short_name)
+
+def edit_book():
+  return
+
+def remove_book():
+  return
+
+validArgString = "Valid arguments: add, edit, OR remove"
+if len(sys.argv) == 1:
+  print(f"ERROR: no arguments specified\n{validArgString}")
+else:
+  arg1 = sys.argv[1].lower()
+  if arg1 == "add":
+    add_book()
+  elif arg1 == "edit":
+    edit_book()
+  elif arg1 == "remove":
+    remove_book()
+  else:
+    print(f"ERROR: invalid argument\n{validArgString}")
