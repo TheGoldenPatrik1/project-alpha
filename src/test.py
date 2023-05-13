f85df47450b980aac72e4dbe47a3890aed77230d
TheGoldenPatrik1 <55322484+TheGoldenPatrik1@users.noreply.github.com>
2023-05-13 12:47:14 -0500

    Test CLI handling w/ txt file

--- src/test.py
+++ src/test.py
@@ -1,8 +1,22 @@
 import sys
+import json
 
-print(len(sys.argv))
-if len(sys.argv) > 1:
-  arg1 = sys.argv[1]
-  print(arg1)
-  if arg1 == "yes":
-    print("correct arg1")
+with open('books.txt') as f:
+  books = json.load(f)
+  book_list = list(books.keys())
+  if len(sys.argv) > 1:
+    arg1 = sys.argv[1]
+    if arg1.isdigit():
+      arg1 = int(arg1)
+      if arg1 >= len(book_list):
+        print(f"ERROR: {arg1} is greater than length of book list ({len(book_list)})")
+      else:
+        book = book_list[arg1]
+        print(f"getting data for {book['title']}")
+    else:
+      arg1 = arg1.lower()
+      if arg1 in book_list:
+        book = book_list[arg1]
+        print(f"getting data for {book['title']}")
+      else:
+        print(f"ERROR: {arg1} is not in book list")
