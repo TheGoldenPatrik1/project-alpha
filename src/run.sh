8dcd991b94290258099a483a5416942a925ac2eb
TheGoldenPatrik1 <55322484+TheGoldenPatrik1@users.noreply.github.com>
2023-05-13 12:14:17 -0500

    Time limit + test

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 #SBATCH --mem-per-cpu=8G
-module load python/python3/3.9.6
-python script.py
+#SBATCH -t 3-00:00
+echo "First arg: $1"
+#module load python/python3/3.9.6
+#python script.py
